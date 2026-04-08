a1e6c7b236928b3b0bcf3fc67e265a3588f2f76c
JPMuro <52582243+JPMuro@users.noreply.github.com>
2026-04-08 19:02:26 -0300

    Update Deatb.cs

--- Assets/Deatb.cs
+++ Assets/Deatb.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement; // IMPORTANTE
 
 public class KillOnWall : MonoBehaviour
 {
     public GameObject textoPerdiste;
 
+    private bool gameOver = false;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Wall")
         {
-            textoPerdiste.SetActive(true); // muestra el texto
-            Time.timeScale = 0f; // pausa el juego
-            gameObject.SetActive(false); // desactiva jugador
+            textoPerdiste.SetActive(true);
+            Time.timeScale = 0f;
+            gameObject.SetActive(false);
+            gameOver = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1f; // reanuda el tiempo
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // reinicia
         }
     }
 }
