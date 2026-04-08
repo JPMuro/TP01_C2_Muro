b3d98d75d8342a9e4355f15575ed8c54071911af
JPMuro <52582243+JPMuro@users.noreply.github.com>
2026-04-08 18:36:30 -0300

    Upload project

--- Assets/Deatb.cs
+++ Assets/Deatb.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using TMPro;
 
 public class KillOnWall : MonoBehaviour
 {
+    public GameObject textoPerdiste;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Wall")
         {
-            gameObject.SetActive(false);
-
-            Time.timeScale = 0f;
+            textoPerdiste.SetActive(true); // muestra el texto
+            Time.timeScale = 0f; // pausa el juego
+            gameObject.SetActive(false); // desactiva jugador
         }
     }
 }
